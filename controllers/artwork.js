aba429f1769fc1a347e37ee5e713f15ad341fc82
justinraj89 <rajewskijustin@gmail.com>
2022-08-15 23:19:58 -0700

    added media query for mobile

--- controllers/artwork.js
+++ controllers/artwork.js
@@ -1,27 +1,9 @@
 const Artwork = require('../models/artwork');
 
 
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
 
 function index(req, res) {
     Artwork.find({}, function(err, allArt){
-        allArt =shuffle(allArt)
         if(err) {
             res.send('You Have an Error')
         }
